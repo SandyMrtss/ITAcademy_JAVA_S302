5952bbcadc3d69655598daf2bbfc3ac15f2d6034
LeandroJames <leandroespanol@gmail.com>
2023-11-22 13:10:27 +0100

    20/11 S302 n2 acabado (falta revisar)

---
+++ src/main/java/n3exercici1/LoadArticles.java
@@ -0,0 +1,10 @@
+package n3exercici1;
+
+import java.io.File;
+import java.util.*;
+
+public class LoadArticles {
+    private Map<String, Double> myArticles;
+    private static final String currDir = System.getProperty("user.dir");
+    private static final File myArticlesData = new File(currDir + "/src/main/java/n3exercici1/articles.csv");
+}
